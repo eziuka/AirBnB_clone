f990d01dd1f21ee04ecbb67fa9f00f54c64596aa
eziuka <eziukajustice@gmail.com>
2023-08-13 09:03:17 -0700

    model files

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for the Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class representing a Amenity."""
+    name = ""
